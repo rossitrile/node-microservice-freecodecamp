1c18406ed3ddf272136738b8556c1255a037baff
Rossi tri le <rossml1993@gmail.com>
2019-02-23 23:08:32 +1100

    Fixing config file

--- services/timestamp/timestampView.js
+++ services/timestamp/timestampView.js
@@ -5,7 +5,7 @@ const router = require("express").Router();
 router.get("/", (req, res) => {
   res.render("timestamp", {
     project: "Time Stamp Microservice",
-    endpoint: `GET https://pacific-tundra-51108.herokuapp.com/api/timestamp/:date_string`
+    endpoint: `GET http://${global.gConfig.hostName}/api/timestamp/:date_string`
   });
 });
 module.exports = router;
