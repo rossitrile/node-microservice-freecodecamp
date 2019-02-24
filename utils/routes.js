43de3b6915f08e5ac2b93f1beb09d674bcb6a236
Rossi tri le <rossml1993@gmail.com>
2019-02-24 11:13:55 +1100

    Header Parser Microservice

--- utils/routes.js
+++ utils/routes.js
@@ -1,4 +1,6 @@
 module.exports = app => {
-  app.use("/api/timestamp", require("../services/timestamp").route);
-  app.use("/timestamp", require("../services/timestamp").view);
+  app.use("/api/timestamp", require("../services/Timestamp").route);
+  app.use("/timestamp", require("../services/Timestamp").view);
+  app.use("/api/whoami", require("../services/HeaderParser").route);
+  app.use("/header-parser", require("../services/HeaderParser").view);
 };
